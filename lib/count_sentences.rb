d5ed0bdc16acffd308fb321d02aba721f6d1f930
Rianna Castro <riannaxcastro@gmail.com>
2021-03-14 21:20:18 -1000

    commit tree 33db0ea383b6e369d41f127373ba36e8c8e7c71c

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
+    if self.end_with?(".")
+      return true
+    else
+  
+      return false
+    end
+
     
   end
 
   def question?
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
 
   end
 
   def exclamation?
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end 
 
   end
 
   def count_sentences
+    new_array = self.split(/[!.?]+/)
+    new_array.length
 
   end
 end
